420240e2e959fe3de04c0e02f311cccc3ba8ce7c
Carson Rey <c.rey494@gmail.com>
2018-09-12 17:17:34 +0000

    automatically backed up by learn

--- countdown.rb
+++ countdown.rb
@@ -1,4 +1,14 @@
-#write your code here
 
-def countdown
+def countdown(integer)
+  while integer > 0 
+    puts "#{integer} SECOND(S)!"
+    integer -= 1 
+  end
+"HAPPY NEW YEAR!"
+end
+
+def countdown_with_sleep(integer)
+  while integer > 0 
+  puts true
+end
 end
